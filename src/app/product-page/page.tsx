43ed8520416959c73f3336e8537ee09468c37bb7
Sergeydigl3 <26508358+Sergeydigl3@users.noreply.github.com>
2024-05-29 14:40:06 +0300

    save

---
+++ src/app/product-page/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Text } from '@/components/ui/text';
+import { Button } from "@/components/ui/button";
+import { Card } from '@/components/ui/card';
+import Image from 'next/image'
+import { ProductCardPage } from '@/components/component/product-card-page';
+
+const ProductPage = () => {
+    return (
+      <ProductCardPage />
+    );
+  };
+
+export default ProductPage;
